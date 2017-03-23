cdf83515113ce6a29d0fc16f96bfdb36b95cff72
Aarju Goyal <goyalaarju@gmail.com>
2017-03-23 21:58:23 +0530

    First commit. Visualisation of data done

---
+++ ML_iris_flower.py
@@ -0,0 +1,40 @@
+# Load libraries
+import pandas
+from pandas.tools.plotting import scatter_matrix
+import matplotlib.pyplot as plt
+from sklearn import model_selection
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+
+print('libraries imported succefully')
+
+
+# Load dataset
+url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
+names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
+dataset = pandas.read_csv(url, names=names)
+#print(dataset)
+
+#shape
+print 'Shape of dataset is',dataset.shape
+
+###peek at 20 values
+##print dataset.head(20)
+
+
+# histograms
+dataset.hist()
+plt.show()
+
+
+#scatter plot matrix
+scatter_matrix(dataset)
+plt.show()
+
